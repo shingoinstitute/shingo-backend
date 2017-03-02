f70ad32a5931852765d67b891dfeba00821e9e5d
sysMachinist <case.haws@usu.edu>
2017-03-02 14:03:17 -0700

    Saving Workable Function before abstracting

--- routes/api/salesforce/awards/publication.js
+++ routes/api/salesforce/awards/publication.js
@@ -3,45 +3,21 @@
 var router = require('express').Router(),
   Promise = require('bluebird'),
   SF = Promise.promisifyAll(require('../../../../models/sf')),
-  cache = Promise.promisifyAll(require('../../../../models/cache'));
-
-var file = 'publication_awards'
+  cache = Promise.promisifyAll(require('../../../../models/cache')),
+  query = Promise.promisifyAll(require('./get'));
 
 // Default Get Route
 // Abstract into single function?
 router.route('/')
 .get(function(req, res, next){
-  var filename = file;
-  var force_refresh = req.query.force_refresh ? req.query.force_refresh : false;
-  if(cache.needsUpdated(filename, 30) || force_refresh){
-    var query = "Select Name, Cover_Image__c, Order_Url__c, Published__c, Public_Author_Name__c, Embedded_Youtube_Url__c, Press_Release_Copy__c FROM Publication_Award__c WHERE Published__c=true"
-    SF.queryAsync(query)
-      .then(function(results){
-        var response = {
-          success: true,
-          records: results.records,
-          total_size: results.totalSize,
-          done: results.done,
-          next_records: results.nextRecordsUrl
-        }
-
-        res.json(response);
-        return cache.addAsync(filename, response);
-      })
-      .then(function(){
-        // TODO Add winston as a logger?
-        console.log("Cache Updated! Filename: ", filename);
-      })
-      .catch(function(err){
-        res.json({
-          success: false,
-          error: err
-        })
-      })
-  }
-  else {
-    res.json(cache[filename]);
-  }
+  var filename = 'publication_awards';
+  var query = "Select Name, Cover_Image__c, Order_Url__c, Published__c, Public_Author_Name__c, Embedded_Youtube_Url__c, Press_Release_Copy__c FROM Publication_Award__c WHERE Published__c=true";
+  var force_refresh = req.query.force_refresh ? req.query.force_refresh : true;
+  query.getQueryAsync(filename, query, force_refresh)
+  .then(function(response){
+    console.log("Get Function: ", response)
+    res.json(response);
+  })
 })
 
 // Get overflow records
