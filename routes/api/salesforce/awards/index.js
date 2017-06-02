21313e6b9d448968d9202a57cf47fd1267c04195
Dustin E. Homan <dustin.e.homan@gmail.com>
2017-06-02 10:23:39 -0600

    Added route for research award

--- routes/api/salesforce/awards/index.js
+++ routes/api/salesforce/awards/index.js
@@ -1,6 +1,8 @@
 var router = require('express').Router(),
-  publication_route = require('./publication');
+  publication_route = require('./publication'),
+  research_route = require('./research');
 
-router.use('/publication', publication_route)
+router.use('/publication', publication_route);
+router.use('/research', research_route);
 
 module.exports = router;
