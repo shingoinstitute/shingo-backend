1b9addeddb09eb63fdf34cdb235056a7ecefa823
Abraham White <abelincoln.white@gmail.com>
2018-07-02 14:59:59 -0600

    Remove unused routes

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,10 @@
 
 var express = require('express'),
   router = express.Router(),
-  path = require('path'),
-  insight_route = express.Router(),
-  support_route = express.Router(),
   api_route = require('./api'),
   subdomain = require('express-subdomain');
 
-insight_route.get('/', function(req, res, next){
-  res.sendFile('/var/www/public/insight-app/index.html');
-});
-
-support_route.get('/', function(req, res, next){
-  res.sendFile('/var/www/public/support-app/index.html');
-});
-
-router.use(subdomain('insight', insight_route));
 router.use(subdomain('api', api_route));
-router.use(subdomain('support', support_route));
-
-router.use(function(req, res){
-  res.redirect('api.shingo.org');
-});
+router.use('/api', api_route);
 
 module.exports = router;
