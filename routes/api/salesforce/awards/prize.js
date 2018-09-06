06c38fd851ad41231f48aaec25ef23764056ead6
Eric Bell <eric4bell@gmail.com>
2018-09-06 15:46:54 -0600

    Removed individual award route, updated prize query for current requirements

--- routes/api/salesforce/awards/prize.js
+++ routes/api/salesforce/awards/prize.js
@@ -9,7 +9,7 @@ var file = 'prize_awards';
 router.route('/')
 .get(function(req, res, next){
   var filename = file;
-  var query = "Select Id, Name, Date_Awarded__c, City__c, State__c, Country__c , SV_Status__c FROM Assessment__c WHERE Publish_to_Website__c=true ORDER BY Date_Awarded__c DESC";
+  var query = "Select Name, Date_Awarded__c, City__c, Country__c, SV_Status__c, Company_Profile_Link__c, Press_Release_Link__c, Years_Since_Awarded__c FROM Assessment__c WHERE Publish_to_Website__c=true and Date_Awarded__c != NULL ORDER BY Date_Awarded__c DESC";
   var force_refresh = req.query.force_refresh ? req.query.force_refresh : false;
   q.getQuery(filename, query, force_refresh, res)
 })
@@ -18,14 +18,6 @@ router.route('/')
 .post(function(req, res) { q.notImplemented(req, res)})
 .put(function(req, res) { q.notImplemented(req, res)})
 .delete(function(req, res) { q.notImplemented(req, res)})
-
-router.route('/:id')
-.get(function(req, res, next){
-  var filename = file + "_" + req.params.id;
-  var query = "Select Name, Company_Profile__c, Press_Release_Link__c, Link_to_Logo__c FROM Assessment__c WHERE Publish_to_Website__c=true AND Id='" + req.params.id + "'";
-  var force_refresh = req.query.force_refresh ? req.query.force_refresh : false;
-  q.getQuery(filename, query, force_refresh, res)
-})
 
 router.get('/next/:next_records', function(req, res){
     var filename = file + '_next_' + req.params.next_records;
