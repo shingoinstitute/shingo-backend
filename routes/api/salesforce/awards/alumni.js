42e3fae968050f27f3c3e136e57cb87faf44f1d5
Eric Bell <eric4bell@gmail.com>
2019-01-30 10:53:19 -0700

    Added alumni query

---
+++ routes/api/salesforce/awards/alumni.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var router = require('express').Router(),
+  path = require('path'),
+  q = require(path.join(appRoot, 'models/query'));
+
+var file = 'alumni';
+
+router.route('/')
+.get(function(req, res, next){
+  var filename = file;
+  var query = "Select Id, Name, Account.Name, Title From Contact Where Shingo_Prize_Relationship__c Includes ('Alumni')";
+  var force_refresh = req.query.force_refresh ? req.query.force_refresh : false;
+  q.getQuery(filename, query, force_refresh, res)
+})
+
+.patch(function(req, res) { q.notImplemented(req, res)})
+.post(function(req, res) { q.notImplemented(req, res)})
+.put(function(req, res) { q.notImplemented(req, res)})
+.delete(function(req, res) { q.notImplemented(req, res)})
+
+// Get overflow records for publication award
+router.get('/next/:next_records', function(req, res){
+  var filename = file + '_next_' + req.params.next_records;
+  var query = req.params.next_records;
+  var force_refresh = req.query.force_refresh ? req. force_refresh : false;
+  q.getQuery(filename, query, force_refresh, res)
+})
+
+module.exports = router;
--- routes/api/salesforce/awards/index.js
+++ routes/api/salesforce/awards/index.js
@@ -2,9 +2,11 @@ var router = require('express').Router(),
   prize_route = require('./prize'),
   publication_route = require('./publication'),
   research_route = require('./research');
+  alumni_route = require('./alumni');
 
 router.use('/prize', prize_route);
 router.use('/publication', publication_route);
 router.use('/research', research_route);
+router.use('/alumni', alumni_route);
 
 module.exports = router;
